099e809f0d3260931321a6cb4b25b6856287ea80
longhoang94 <longsutls@gmail.com>
2018-12-03 08:43:43 +0700

    test hello world

--- src/main/java/com/longhoang/finalexammodule2/controllers/EmployeeController.java
+++ src/main/java/com/longhoang/finalexammodule2/controllers/EmployeeController.java
@@ -1,12 +1,14 @@
 package com.longhoang.finalexammodule2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/employee/")
+@RequestMapping("/employee")
 public class EmployeeController {
+    @GetMapping("/")
     public ModelAndView index() {
         return new ModelAndView("list");
     }
