4e46ddecbfb392d7983178554fa6f49759a058ab
longhoang94 <longsutls@gmail.com>
2018-12-03 08:42:54 +0700

    first commit

--- src/main/java/com/longhoang/finalexammodule2/models/Employee.java
+++ src/main/java/com/longhoang/finalexammodule2/models/Employee.java
@@ -14,4 +14,72 @@ public class Employee {
     private String idCard;
     private String email;
     private String address;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String gender, String dayOfBirth, String idCard, String email, String address) {
+        this.name = name;
+        this.gender = gender;
+        this.dayOfBirth = dayOfBirth;
+        this.idCard = idCard;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(String dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
